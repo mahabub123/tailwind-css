656d2f6bfea78539c5f477b8a3c8b23fe2717fc3
mytecnic <mahabubalq@gmail.com>
2022-02-15 07:47:37 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      container:{
+        center:true,
+        padding:'1rem',
+        screens:{
+          lg:'1124px'
+        }
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
